a2e755a3ec5eac35638b7136608dcbf9b74ab75d
manee2k6 <manee2k6@gmail.com>
2018-03-22 09:14:55 +0530

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,20 +3,12 @@
 # this script is only tested on ubuntu xenial
 
 # install docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce
+apt-get install -y docker-engine
+systemctl enable docker
+systemctl start docker
 usermod -aG docker ubuntu
 
 # run jenkins
